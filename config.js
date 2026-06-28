c7f1c1591e1aa46aa3de4fb135693a56630f5982
JigBaker-rsp <jb.boisnault@gmail.com>
2026-06-28 13:21:18 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@
 
 window.APP_CONFIG = {
   // Supabase > Project Settings > Data API
-  SUPABASE_URL: "https://TON-PROJET.supabase.co",
-  SUPABASE_PUBLISHABLE_KEY: "TON_ANON_OU_PUBLISHABLE_KEY",
+  SUPABASE_URL: "https://tunhsahqvdaorcnzypue.supabase.co",
+  SUPABASE_PUBLISHABLE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1bmhzYWhxdmRhb3Jjbnp5cHVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI2MjU4MjYsImV4cCI6MjA5ODIwMTgyNn0.Ij3AZp9BclPkFTxJHDTClpQWl1xbqLoD39u0xiHmw4Q",
 
   // Nom de la table créée par supabase.sql
   TABLE_NAME: "sorge_presences"
